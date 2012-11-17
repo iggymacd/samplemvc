24a630c9a0ec436810a862d3ddd63a7dcdc9ee44
Allan <iggymacd@gmail.com>
2012-11-17 09:29:01 -0400

    added support for game isolate

--- web/model.dart
+++ web/model.dart
@@ -1,6 +1,8 @@
 library model;
 import 'dart:math';
+import 'dart:isolate';
 import 'playing_cards.dart';
+import 'package:web_components/watcher.dart';
 //import 'package:samplemvc/playing_cards.dart';
 //import 'playing_cards.dart';
 var _app;
@@ -11,13 +13,22 @@ App get app{
   }
   return _app;
 }
+gameIsolate() {
+  port.receive((msg, replyTo) {
+    print('doing some work ${app.cards.length}');
+    if (replyTo != null) replyTo.send(msg);
+  });
+}
 
 class App {
   List<Card> cards;
   Map<String,Deck> decks;
   List<String> positions = ['south','west', 'north', 'east'];
+  Timer timer;
+  //Game currentGame;
   num dealer;
   num nextToPlay;
+  var sender;
   App(){
     cards = createDeck();
     decks = new Map<String,Deck>();
@@ -30,23 +41,48 @@ class App {
     decks['round'] = new Deck('round', new List<Card>());
     dealer = new Random().nextInt(positions.length);
     nextToPlay = (dealer + 1) % 4;
-    decks[positions[dealer]].isDealer = true;
+    decks[positions[dealer]].isDealer = true; 
     decks[positions[nextToPlay]].isNextToPlay = true;
+    //currentGame = new Game();
     //cards = shuffle(cards);
   }
-  void dealCards(){
+  void startGame(){
     num nextPlayer = dealer + 1;
     List cardsToDeal = decks['dealer'].cards;
     num numberOfCards = cardsToDeal.length;
     //print('there are ${numberOfCards} cards to deal.');
     Card currentCard;
+    //timer = new Timer.repeating(1000, (Timer timer) => runMonitor());
     for(num i = 0; i < numberOfCards ; i++){
-      currentCard = cardsToDeal.removeLast();
+      Card currentCard = cardsToDeal.removeLast();
       num temp = nextPlayer % 4;
-      //print(temp);
-      decks[positions[temp]].addCard(currentCard);
+//      new Timer(500, (Timer timer){
+        decks[positions[temp]].addCard(currentCard);
+//        dispatch();
+//        print('here');
+//      });
+      
       nextPlayer++;
     }
+
+
+    sender = spawnFunction(gameIsolate);
+    var receiver = new ReceivePort();
+    receiver.receive((Message msg, _) {
+      if (msg.type == Message.MESSAGE) {
+        print('shutting down');
+        print(msg.from);
+        print(msg.message);
+        print(msg.toMap());
+        //receiver.close();
+      }
+    });
+    sender.send(new Message('north', 'shutdown'), receiver.toSendPort());
+    
+  }
+  
+  void tranferCard(Card source, Deck target){
+    target.addCard(source);
   }
 }
 
@@ -139,4 +175,71 @@ List shuffle(List myArray) {
   }
 
   return myArray;
+}
+class Message {
+  static final int JOIN = 0;
+  static final int MESSAGE = 1;
+  static final int LEAVE = 2;
+  static final int TIMEOUT = 3;
+  static final int START = 4;
+  static final int STOP = 5;
+  static final int STARTING = 6;
+  static final int STOPPING = 7;
+  static final int WAITING = 8;
+  static final int STARTED = 9;
+  static final int PROFILE = 10;
+  static final int UI_READY = 11;
+  static final List<String> _typeName =
+      const [ "join", "message", "leave", "timeout", "start", "stop", 
+              "starting", "stopping", "waiting", "started", "profile", "uiReady"];
+
+  Message.start(this._from)
+  : _received = new Date.now(), _type = START;
+  Message.starting(this._from)
+  : _received = new Date.now(), _type = STARTING;
+  Message.started(this._from)
+  : _received = new Date.now(), _type = STARTED;
+  Message.stop(this._from)
+  : _received = new Date.now(), _type = STOP;
+  Message.stopping(this._from)
+  : _received = new Date.now(), _type = STOPPING;
+  Message.join(this._from)
+      : _received = new Date.now(), _type = JOIN;
+  Message(this._from, this._message)
+  : _received = new Date.now(), _type = MESSAGE;
+//  Message.profile(this._from, this._handle)
+//      : _received = new Date.now(), _type = PROFILE;
+  Message.leave(this._from)
+      : _received = new Date.now(), _type = LEAVE;
+  Message.timeout(this._from)
+  : _received = new Date.now(), _type = TIMEOUT;
+  Message.waiting(this._from)
+  : _received = new Date.now(), _type = WAITING;
+  Message.uiReady(this._from)
+      : _received = new Date.now(), _type = UI_READY;
+
+  String get from => _from;
+  Date get received => _received;
+  String get message => _message;
+  int get type => _type;
+  //void set messageNumber(int n) => _messageNumber = n;
+
+  Map toMap() {
+    Map map = new Map();
+    map["from"] = _from;
+    map["received"] = _received.toString();
+    map["type"] = _type;
+    map["typeName"] = _typeName[_type];
+    if (_type == MESSAGE) map["message"] = _message;
+    //if (_type == PROFILE) map["handle"] = _handle;
+    //map["number"] = _messageNumber;
+    return map;
+  }
+
+  String _from;
+  Date _received;
+  int _type;
+  String _message;
+  //String _handle;
+  //int _messageNumber;
 }
